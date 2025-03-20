256e9927387610e59b486799d24ab1bcf4c71321
Alejandro R Pascual <alejandrorpascual@Alejandros-MacBook-Pro.local>
2025-03-20 12:18:32 -0400

    chapter 1 done

--- hello/main.go
+++ hello/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	var lang string
+	flag.StringVar(&lang, "lang", "en", "The required language, e.g. en, ur...")
+	flag.Parse()
+
+	greeting := greet(language(lang))
+	fmt.Println(greeting)
+}
+
+type language string
+
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+
+}
+
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
 }
--- hello/main_internal_test.go
+++ hello/main_internal_test.go
@@ -1,5 +1,59 @@
 package main
 
+import "testing"
+
+func TestGreet(t *testing.T) {
+	type testCase struct {
+		lang language
+		want string
+	}
+
+	var tests = map[string]testCase{
+		"English": {
+			lang: language("en"),
+			want: "Hello world",
+		},
+		"French": {
+			lang: language("fr"),
+			want: "Bonjour le monde",
+		},
+		"Akkadian, not supported": {
+			lang: language("akk"),
+			want: `unsupported language: "akk"`,
+		},
+		"Greek": {
+			lang: language("el"),
+			want: "Χαίρετε Κόσμε",
+		},
+		"Hebrew": {
+			lang: language("he"),
+			want: "שלום עולם",
+		},
+		"Urdu": {
+			lang: language("ur"),
+			want: "ہیلو دنیا",
+		},
+		"Vietnamese": {
+			lang: language("vi"),
+			want: "Xin chào Thế Giới",
+		},
+		"Empty": {
+			lang: language(""),
+			want: `unsupported language: ""`,
+		},
+	}
+
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := greet(tc.lang)
+			if got != tc.want {
+				t.Errorf("expected: %q, got: %q", tc.want, got)
+			}
+		})
+	}
+}
+
 func ExampleMain() {
 	main()
 	// Output:
