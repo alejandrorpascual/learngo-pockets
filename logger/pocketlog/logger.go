ca218ff0cf763ee9f4a643be018b775970268517
alejandrorpascual <alexandros2707@gmail.com>
2025-05-12 11:37:20 -0400

    api progress

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -2,3 +2,18 @@ package pocketlog
 
 type Logger struct {
 }
+
+// Debugf formats and prints a message if the log level is DEBUG or higher
+func (l *Logger) Debugf(format string, args ...any) {
+	// implement
+}
+
+// Infof formats and prints a message if the log level is INFO or higher
+func (l *Logger) Infof(format string, args ...any) {
+	// implement
+}
+
+// Errorf formats and prints a message if the log level is ERROR or higher
+func (l *Logger) Errorf(format string, args ...any) {
+	// implement
+}
