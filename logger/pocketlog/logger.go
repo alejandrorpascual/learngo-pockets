eb955892880144ff24e77966b075c14cf618ad53
alejandrorpascual <alexandros2707@gmail.com>
2025-05-13 12:18:13 -0400

    first implementation done

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -1,19 +1,41 @@
 package pocketlog
 
+import "fmt"
+
 type Logger struct {
+	threshold Level
 }
 
-// Debugf formats and prints a message if the log level is DEBUG or higher
+// New returns you a logger, ready to log at the required threshold.
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+// Debugf formats and prints a message if the log level is DEBUG or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	// implement
+	if l.threshold > LevelDebug {
+		return
+	}
+
+	fmt.Printf(format+"\n", args...)
 }
 
-// Infof formats and prints a message if the log level is INFO or higher
+// Infof formats and prints a message if the log level is INFO or higher.
 func (l *Logger) Infof(format string, args ...any) {
-	// implement
+	if l.threshold > LevelInfo {
+		return
+	}
+
+	fmt.Printf(format+"\n", args...)
 }
 
-// Errorf formats and prints a message if the log level is ERROR or higher
+// Errorf formats and prints a message if the log level is ERROR or higher.
 func (l *Logger) Errorf(format string, args ...any) {
-	// implement
+	if l.threshold > LevelError {
+		return
+	}
+
+	fmt.Printf(format+"\n", args...)
 }
