4dda1b1d99a278287da6e1884683dbd3a8e013a8
alejandrorpascual <alexandros2707@gmail.com>
2025-05-14 11:05:25 -0400

    intial setup of gordle game

---
+++ gordle/gordle/game.go
@@ -0,0 +1,20 @@
+package gordle
+
+import "fmt"
+
+// Game holds all the information we need to play a game of gordle.
+type Game struct {
+}
+
+// New returns a game, which can be used to Play!
+func New() *Game {
+	g := &Game{}
+
+	return g
+}
+
+func (g *Game) Play() {
+	fmt.Println("Welcome to Gordle!")
+
+	fmt.Printf("Enter a guess:\n")
+}
