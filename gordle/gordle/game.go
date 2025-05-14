4a330826cf9aee2cda17c8844d9c227652fa7adf
alejandrorpascual <alexandros2707@gmail.com>
2025-05-14 11:57:13 -0400

    first attempt at ask method

--- gordle/gordle/game.go
+++ gordle/gordle/game.go
@@ -1,14 +1,24 @@
 package gordle
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+const solutionLength = 5
 
 // Game holds all the information we need to play a game of gordle.
 type Game struct {
+	reader *bufio.Reader
 }
 
 // New returns a game, which can be used to Play!
-func New() *Game {
-	g := &Game{}
+func New(playerInput io.Reader) *Game {
+	g := &Game{
+		reader: bufio.NewReader(playerInput),
+	}
 
 	return g
 }
@@ -16,5 +26,27 @@ func New() *Game {
 func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 
-	fmt.Printf("Enter a guess:\n")
+	guess := g.ask()
+
+	fmt.Printf("Your guess is: %s\n", string(guess))
+}
+
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+
+	for {
+		playerAnswer, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %s\n", err.Error())
+			continue
+		}
+
+		guess := []rune(string(playerAnswer))
+
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution!\nExpected %d characters, got %d.\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
--- gordle/main.go
+++ gordle/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "learngo-pockets/gordle/gordle"
+import (
+	"learngo-pockets/gordle/gordle"
+	"os"
+)
 
 func main() {
-	g := gordle.New()
+	g := gordle.New(os.Stdin)
 	g.Play()
 }
